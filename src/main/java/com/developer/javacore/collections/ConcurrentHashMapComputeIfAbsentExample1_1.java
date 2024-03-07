ac40b20aeb84dbfe6d3860681bcf56887694fbeb
Mauricio Del Rei <mauricioaraujodelrei@gmail.com>
2024-03-07 15:56:16 +0000

    ConcurrentHashMap

--- src/main/java/com/developer/javacore/collections/ConcurrentHashMapComputeIfAbsentExample1_1.java
+++ src/main/java/com/developer/javacore/collections/ConcurrentHashMapComputeIfAbsentExample1_1.java
@@ -1,5 +1,8 @@
 package com.developer.javacore.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Java ConcurrentHashMap class
 A hash table supporting full concurrency of retrievals and high expected concurrency for updates.
@@ -9,5 +12,19 @@ However, even though all operations are thread-safe, retrieval operations do not
 This class is fully interoperable with Hashtable in programs that rely on its thread safety but not on
  its synchronization details..
 * */
-public class ConcurrentHashMapExample {
+public class ConcurrentHashMapComputeIfAbsentExample1_1 {
+    public static void main(String[] args) {
+        Map<String,Integer>mapcon = new HashMap<>();
+        mapcon.put("k1", 100);
+        mapcon.put("k2", 200);
+        mapcon.put("k3", 300);
+        mapcon.put("k4", 400);
+        System.out.println("HashMap values :\n " + mapcon.toString());
+        mapcon.computeIfAbsent("k5",k -> 200 + 300);
+        mapcon.computeIfAbsent("k6",k -> 60 * 10);
+        System.out.println("New HashMap after computeIfAbsent :\n "+ mapcon);
+        mapcon.computeIfPresent("k1",(k,v) -> 1000);
+        System.out.println("New HashMap after computeIfPresent :\n "+ mapcon);
+    }
+    
 }
--- src/main/java/com/developer/javacore/collections/ConcurrentHashMapContainsValueExample1_1.java
+++ src/main/java/com/developer/javacore/collections/ConcurrentHashMapContainsValueExample1_1.java
@@ -1,2 +1,20 @@
-package com.developer.javacore.collections;public class ConcurrentHashMapcontainsValueExample1_1 {
+package com.developer.javacore.collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapContainsValueExample1_1 {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap<String,Integer>concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("AAA",10);
+        concurrentHashMap.put("BBB",15);
+        concurrentHashMap.put("CCC",25);
+        concurrentHashMap.put("DDD",255);
+        concurrentHashMap.put("EEE",30);
+
+        System.out.println(" Mappings are: " + concurrentHashMap);
+
+        System.out.println("is 255  present? ::  " + concurrentHashMap.containsValue(255));
+    }
+
 }
